3a3eb1a4586e9546c08f73511f1e5838bc5740d1
jramiro <jramiro@student.42paris.fr>
2025-09-26 18:38:38 +0200

    ex00

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jramiro <jramiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 17:43:14 by jramiro           #+#    #+#             */
-/*   Updated: 2025/09/26 18:35:52 by jramiro          ###   ########.fr       */
+/*   Updated: 2025/09/26 18:38:02 by jramiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,17 +44,22 @@ int	main(void)
 {
 	std::cout << REVERSED TEAL "--- SUCCESSFUL TESTS ---\n" RESET << std::endl;
 
+	int	size;
+
 	std::vector<int> v_vect;
 	int	arr1[] = {1, 9, 2, 8, 3, 7, 42};
-	populateContainer(v_vect, arr1, sizeof(arr1) / sizeof(arr1[0]));
+	size = sizeof(arr1) / sizeof(arr1[0]);
+	populateContainer(v_vect, arr1, size);
 
 	std::list<int> v_list;
 	int	arr2[] = {-12, 12, 42, -42};
-	populateContainer(v_list, arr2, sizeof(arr2) / sizeof(arr2[0]));
+	size = sizeof(arr2) / sizeof(arr2[0]);
+	populateContainer(v_list, arr2, size);
 
 	std::deque<int> v_deq;
 	int	arr3[] = {42, 1, 2, 3, 123};
-	populateContainer(v_deq, arr3, sizeof(arr3) / sizeof(arr3[0]));
+	size = sizeof(arr3) / sizeof(arr3[0]);
+	populateContainer(v_deq, arr3, size);
 
 	testFind(v_vect, 42, "vector");
 	testFind(v_list, 42, "list");
